d43050aee94f6fedc7dc6b3b5686740981c1ae09
Felix Redondo <frmanzanares@hotmail.com>
2024-08-09 22:32:31 +0200

    chore: Update Firebase configuration for hosting and functions

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+import { https } from "firebase-functions";
+import next from "next";
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+export const nextjs = https.onRequest((req, res) => {
+  return app.prepare().then(() => handle(req, res));
+});
